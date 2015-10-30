b46d3628d64ad2f44673094953afa86cb4579285
yang99 <996212017@qq.com>
2015-10-30 13:45:19 +0800

    Initial Commit

---
+++ YYao/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YYao
+//
+//  Created by yangyao on 15/10/30.
+//  Copyright © 2015年 yangyao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
